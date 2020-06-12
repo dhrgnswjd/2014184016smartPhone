047d6f6bf4b90e2667d34bb9388412bdbb08c16d
dhrgnswjd <dhrgnswjd@daum.net>
2020-06-12 22:40:11 +0900

    0612

--- BuildingBreak/app/src/main/java/kr/ac/kpu/game/sdw/buldingbreakproject/ui/view/GameView.java
+++ BuildingBreak/app/src/main/java/kr/ac/kpu/game/sdw/buldingbreakproject/ui/view/GameView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.media.Image;
 import android.util.AttributeSet;
 import android.view.View;
 
